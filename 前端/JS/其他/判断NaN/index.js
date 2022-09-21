c98b777a498dda12d355a978daebca364933aad4
LiangSongWei <46132710+a1371083012@users.noreply.github.com>
2022-09-21 08:29:07 +0800

    更新JS文档：【判断NaN】
    新增：【判断NaN】示例代码

---
+++ 前端/JS/其他/判断NaN/index.js
@@ -0,0 +1,21 @@
+// 常规判断
+console.log("常规判断：");
+let empty = null;
+let under = undefined;
+let nan = NaN;
+let str = "NaN";
+
+console.log(isNaN(empty));  // false
+console.log(isNaN(under));  // true
+console.log(isNaN(nan));    // true
+console.log(isNaN(str));    // true
+
+// 封装判断
+console.log("封装判断：");
+function IsNaN(e){
+    return isNaN(e)&&typeof e == "string";
+}
+console.log(IsNaN(empty));  // false
+console.log(IsNaN(under));  // false
+console.log(IsNaN(nan));    // false
+console.log(IsNaN(str));    // true
