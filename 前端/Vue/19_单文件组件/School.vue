08bd0ac74293a87c4914b014db88ef5635a66794
LiangSongWei <46132710+a1371083012@users.noreply.github.com>
2022-12-13 09:23:25 +0800

    更新Vue：【单文件组件】
    更新：【单文件组件】示例代码

---
+++ 前端/Vue/19_单文件组件/School.vue
@@ -0,0 +1,33 @@
+<!-- 组件的结构 -->
+<template>
+  <div class="demo">
+    <h2>学校名称：{{schoolName}}</h2>
+    <h2>学校地址：{{address}}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+<!-- 组件交互相关的代码（数据、方法等等） -->
+<script>
+  export default {
+    name: 'School',
+    data(){
+      return {
+        schoolName: '北京大学',
+        address: '北京',
+      }
+    },
+    methods: {
+      showName(){
+        console.log(this.schoolName);
+      }
+    }
+  };
+</script>
+
+<!-- 组件的样式 -->
+<style>
+  .demo{
+    background-color: orange;
+  }
+</style>
---
+++ 前端/Vue/19_单文件组件/Student.vue
@@ -0,0 +1,20 @@
+<!-- 组件的结构 -->
+<template>
+  <div>
+    <h2>学生姓名：{{name}}</h2>
+    <h2>学生年龄：{{age}}</h2>
+  </div>
+</template>
+
+<!-- 组件交互相关的代码（数据、方法等等） -->
+<script>
+  export default {
+    name: 'Student',
+    data(){
+      return {
+        name: '张三',
+        age: '18',
+      }
+    }
+  };
+</script>
